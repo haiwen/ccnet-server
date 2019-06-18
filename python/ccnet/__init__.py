fdc360fa4f0cb01dcdb3ff2dbc2c87f1710ccb4c
caixiangyue <xiangyue.cai@seafile.com>
2019-06-18 16:23:46 +0800

    remove unused code

--- python/ccnet/__init__.py
+++ python/ccnet/__init__.py
@@ -1,7 +1,1 @@
-from ccnet.errors import NetworkError
-from ccnet.sync_client import SyncClient
-
-from ccnet.pool import ClientPool
 from ccnet.rpc import CcnetThreadedRpcClient
-
-from ccnet.message import Message
